3ad89fa404cb81a55cb60fd44331fa0996a29660
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-15 13:29:22 +0000

    Create PrimeiraMao Interface class

---
+++ src/Contracts/PrimeiraMao.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PrimeiraMao;
+
+/**
+ * PrimeiraMao SDK
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class PrimeiraMao
+{
+    /**
+     * The Primeira Mão version.
+     *
+     * @var string
+     */
+    const VERSION = '0.0.1';
+    
+    /**
+     * Get the version number of the application.
+     *
+     * @return string
+     */
+    public static function version()
+    {
+        return self::VERSION;
+    }
+}
