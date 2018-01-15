1832fbff6b8185ae6efdd8dfa0ed279ffa9ced46
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-15 13:29:53 +0000

    Create ErrorBag Interface class

---
+++ src/Contracts/Http/ErrorBag.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace PrimeiraMao\Contracts\Http;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface ErrorBag
+{
+    /**
+     * Set data
+     * 
+     * @param array $errors
+     * @return $this
+     */
+    public function setData(array $errors);
+    
+    /**
+     * Get first error
+     * 
+     * @return bool | object
+     */
+    public function first();
+    
+    /**
+     * Get last error
+     * 
+     * @return bool | object
+     */
+    public function last();
+    
+    /**
+     * Check code exists
+     * 
+     * @param int $code
+     * @return bool
+     */
+    public function codeExists(int $code);
+    
+    /**
+     * Get all errors
+     * 
+     * @return array
+     */
+    public function all();
+    
+    /**
+     * Get all error codes
+     * 
+     * @return array
+     */
+    public function codes();
+    
+    /**
+     * Get all error values
+     * 
+     * @return array
+     */
+    public function values();
+}
