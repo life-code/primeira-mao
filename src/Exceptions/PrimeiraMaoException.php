1c14df6967da79ea070a5cbf4a9e36530ae716e9
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-22 12:01:55 +0000

    PrimeiraMaoException class created

---
+++ src/Exceptions/PrimeiraMaoException.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace PrimeiraMao\Exceptions;
+
+use Exception;
+use Throwable;
+use PrimeiraMao\PrimeiraMao;
+use PrimeiraMao\Languages\Language;
+
+/**
+ * PrimeiraMao SDK
+ * 
+ * @type        library
+ * @version     1.0.31
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class PrimeiraMaoException extends Exception
+{
+    /**
+     * Path to language files
+     * 
+     * @var string
+     */
+    private $path = 'Throws';
+    
+    /**
+     * @var \PrimeiraMao\Languages\Language
+     */
+    private $language;
+    
+    /**
+     * Make new instance of this class
+     * 
+     * @param string $message
+     * @param int $code
+     * @param \Throwable $previous
+     * @return void
+     */
+    public function __construct(string $message = null, int $code = null, Throwable $previous = null)
+    {
+        $message = $this->handlerMessage($message, $code);
+        
+        parent::__construct($message, $code, $previous);
+    }
+    
+    /**
+     * Handles message
+     * 
+     * @param string $message
+     * @param int $code
+     * @return string
+     */
+    private function handlerMessage(string $message = null, int $code = null) : string
+    {
+        $translate = $this->getLanguage()->translate((int) $code);
+        
+        return str_replace('#value#', $message, $translate);
+    }
+    
+    /**
+     * Get Language instance
+     * 
+     * @return \PrimeiraMao\Languages\Language
+     */
+    private function getLanguage()
+    {
+        if (!$this->language) {
+            $this->language = new Language($this->getEnv(), $this->getPath());
+        }
+        
+        return $this->language;
+    }
+    
+    /**
+     * Get path
+     * 
+     * @return string
+     */
+    private function getPath() : string
+    {
+        return $this->path;
+    }
+    
+    /**
+     * Get environment
+     * 
+     * @return \PrimeiraMao\Contracts\Credentials\Environment
+     */
+    private function getEnv()
+    {
+        return PrimeiraMao::getEnv();
+    }
+}
