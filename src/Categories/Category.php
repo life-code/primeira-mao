d33557c3647e80501c58308f572e8cf8d5d25a82
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-23 23:28:48 +0000

    Category class created

---
+++ src/Categories/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PrimeiraMao\Categories;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Category
+{
+    
+}
