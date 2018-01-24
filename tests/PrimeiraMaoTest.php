18491521cd495c18ffab1005d3cce102d3ecbc0f
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-24 18:55:00 +0000

    Init Unit Test

---
+++ tests/PrimeiraMaoTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PrimeiraMao\PrimeiraMao;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class PrimeiraMaoTest extends TestCase
+{
+    /**
+     * Test version
+     *
+     * @return void
+     */
+    public function testVersion()
+    {
+        $this->assertEquals('0.0.1', PrimeiraMao::version());
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Primeira Mão
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+
+require __DIR__.'/../vendor/autoload.php';
