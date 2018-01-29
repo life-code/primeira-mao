22c1be8ffccb51086d433a4d492aa8dd5723dedc
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-29 12:20:06 +0000

    Language contract created

---
+++ src/Contracts/Languages/Language.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PrimeiraMao\Contracts\Languages;
+
+/**
+ * PrimeiraMao SDK
+ * 
+ * @type        library
+ * @version     1.0.31
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Language
+{
+    /**
+     * Generate path
+     * 
+     * @param string $path
+     * @return string
+     */
+    public function generatePath(string $path) : string;
+    
+    /**
+     * Translate error value
+     * 
+     * @param int $code
+     * @param string $default
+     * @return string
+     */
+    public function translate(int $code, string $default = '') : string;
+    
+    /**
+     * Get translations
+     * 
+     * @throws \PrimeiraMao\Exceptions\PrimeiraMaoException
+     * @return array
+     */
+    public function getTranslations() : array;
+}
