a8902ca0f78503a857a063d1287c24922c5acc40
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-29 15:16:40 +0000

    Profile attributes class removed

--- src/Profiles/Profile.php
+++ src/Profiles/Profile.php
@@ -2,9 +2,6 @@
 
 namespace PrimeiraMao\Profiles;
 
-use Datetime;
-use PrimeiraMao\Auth\User;
-
 /**
  * PrimeiraMao API
  * 
@@ -17,53 +14,5 @@ use PrimeiraMao\Auth\User;
  */
 class Profile
 {
-    /**
-     * @var int
-     */
-    private $migrate_id;
     
-    /**
-     * @var string
-     */
-    private $href;
-    
-    /**
-     * @var id
-     */
-    private $id;
-    
-    /**
-     * @var string
-     */
-    private $name;
-    
-    /**
-     * @var string
-     */
-    private $nickname;
-    
-    /**
-     * @var string
-     */
-    private $birthday;
-    
-    /**
-     * @var string
-     */
-    private $cpf;
-    
-    /**
-     * @var \PrimeiraMao\Auth\User
-     */
-    private $user;
-    
-    /**
-     * @var \Datetime
-     */
-    private $created_at;
-    
-    /**
-     * @var \Datetime
-     */
-    private $updated_at;
 }
