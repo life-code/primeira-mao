944e5a5fc2badb50386b4b17fedbfbf91905a430
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-29 17:25:19 +0000

    Find, delete, update and all methods created in Profile class

--- src/Profiles/Profile.php
+++ src/Profiles/Profile.php
@@ -2,6 +2,10 @@
 
 namespace PrimeiraMao\Profiles;
 
+use PrimeiraMao\Http\Request;
+use PrimeiraMao\Contracts\Http\Response;
+use PrimeiraMao\Contracts\Repository;
+
 /**
  * PrimeiraMao API
  * 
@@ -12,7 +16,68 @@ namespace PrimeiraMao\Profiles;
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
  * @license     MIT
  */
-class Profile
+class Profile implements Repository
 {
+    /**
+     * @var string
+     */
+    private $path = 'profiles';
     
+    /**
+     * Find one register
+     * 
+     * @param int $id
+     * @return \PrimeiraMao\Http\Response
+     */
+    public function find(int $id) : Response
+    {
+        $request = new Request(Request::GET, $this->path . '/' . $id . '.json');
+        
+        return $request->send();
+    }
+    
+    /**
+     * Delete one register
+     * 
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id) : bool
+    {
+        $request = new Request(Request::DELETE, $this->path . '/' . $id . '.json');
+        
+        return $request->send();
+    }
+    
+    /**
+     * Update one register
+     * 
+     * @param int $id
+     * @param array $data
+     * @return \PrimeiraMao\Http\Response
+     */
+    public function update(int $id, array $data) : Response
+    {
+        $request = new Request(Request::PATCH, $this->path . '/' . $id . '.json');
+        
+        return $request->setData($data)->send();
+    }
+    
+    /**
+     * Get all registers with paginate
+     * 
+     * @param int $migrate_id
+     * @param int $page
+     * @param int $limit
+     * @param bool $status
+     * @return \PrimeiraMao\Http\Response
+     */
+    public function all(int $migrate_id = 0, int $page = 0, int $limit = 10, bool $status = true) : Response
+    {
+        $appends = ['migrate_id' => $migrate_id, 'page' => $page, 'limit' => $limit, 'status' => $status];
+        
+        $request = new Request(Request::GET, $this->path . '.json');
+        
+        return $request->appends($appends)->send();
+    }
 }
