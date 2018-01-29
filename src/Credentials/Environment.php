7b2b9d03ed1d724b05f236a9823a8d59ee34a5aa
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-29 15:13:48 +0000

    Environment class changed

--- src/Credentials/Environment.php
+++ src/Credentials/Environment.php
@@ -9,10 +9,10 @@ use PrimeiraMao\Languages\Location;
 use Dotenv\Dotenv;
 
 /**
- * PrimeiraMao SDK
+ * PrimeiraMao API
  * 
  * @type        library
- * @version     1.0.31
+ * @version     0.0.1
  * @package     life-code/primeira-mao
  * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
