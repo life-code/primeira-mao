807c67f768efe5bdd1cf88911651f65328930992
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-01-29 11:49:25 +0000

    User class created

---
+++ src/Auth/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PrimeiraMao\Auth;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class User
+{
+    /**
+     * @var int
+     */
+    public $email;
+    
+    /**
+     * @var string
+     */
+    public $password;
+}
