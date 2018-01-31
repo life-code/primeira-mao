aa31f1b12411e438fd9fb7d435ce2ba9bee148db
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-01-31 19:03:27 +0000

    Interface added in Ad class

--- src/Ads/Ad.php
+++ src/Ads/Ad.php
@@ -4,6 +4,7 @@ namespace PrimeiraMao\Ads;
 
 use PrimeiraMao\Http\Request;
 use PrimeiraMao\Contracts\Http\Response;
+use PrimeiraMao\Contracts\Repository;
 
 /**
  * PrimeiraMao API
@@ -15,7 +16,86 @@ use PrimeiraMao\Contracts\Http\Response;
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
  * @license     MIT
  */
-class Ad
+class Ad implements Repository
 {
+    /**
+     * @var string
+     */
+    private $url = 'ads';
     
+    /**
+     * Get all registers with paginate
+     * 
+     * @param array $appends
+     * @return \PrimeiraMao\Contracts\Http\Response
+     */
+    public function all(array $appends = []) : Response
+    {
+        $url = $this->url . '.json';
+        
+        $request = new Request(Request::GET, $url, $this->url);
+        
+        return $request->appends($appends)->send();
+    }
+    
+    /**
+     * Find one register
+     * 
+     * @param int $id
+     * @return \PrimeiraMao\Contracts\Http\Response
+     */
+    public function find(int $id) : Response
+    {
+        $url = $this->url . '/' . $id . '.json';
+        
+        $request = new Request(Request::GET, $url, $this->url);
+        
+        return $request->send();
+    }
+    
+    /**
+     * Create one register
+     * 
+     * @param array $data
+     * @return \PrimeiraMao\Contracts\Http\Response
+     */
+    public function create(array $data) : Response
+    {
+        $url = $this->url . '.json';
+        
+        $request = new Request(Request::POST, $url, $this->url);
+        
+        return $request->setData($data)->send();
+    }
+    
+    /**
+     * Update one register
+     * 
+     * @param int $id
+     * @param array $data
+     * @return \PrimeiraMao\Contracts\Http\Response
+     */
+    public function update(int $id, array $data) : Response
+    {
+        $url = $this->url . '/' . $id . '.json';
+        
+        $request = new Request(Request::PATCH, $url, $this->url);
+        
+        return $request->setData($data)->send();
+    }
+    
+    /**
+     * Delete one register
+     * 
+     * @param int $id
+     * @return \PrimeiraMao\Contracts\Http\Response
+     */
+    public function delete(int $id) : Response
+    {
+        $url = $this->url . '/' . $id . '.json';
+        
+        $request = new Request(Request::DELETE, $url, $this->url);
+        
+        return $request->send();
+    }
 }
