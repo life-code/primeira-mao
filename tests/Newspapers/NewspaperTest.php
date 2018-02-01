fda847c7edf72c7528e091c62ac628380768eeb5
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-01 12:36:25 +0000

    NewspaperTest class created with CRUD test methods

---
+++ tests/Newspapers/NewspaperTest.php
@@ -0,0 +1,89 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PrimeiraMao\Newspapers\Newspaper;
+use PrimeiraMao\Http\Response;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class NewspaperTest extends TestCase
+{
+    /**
+     * Customer Instance
+     * 
+     * @return \PrimeiraMao\Newspapers\Newspaper
+     */
+    public static function instance()
+    {
+        return new Newspaper();
+    }
+    
+    /**
+     * Test instance account credentials
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Newspaper::class, $this->instance());
+    }
+    
+    /**
+     * Test method all
+     *
+     * @return void
+     */
+    public function testAll()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->all());
+    }
+    
+    /**
+     * Test method find
+     *
+     * @return void
+     */
+    public function testFind()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->find(1));
+    }
+    
+    /**
+     * Test method create
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->create([]));
+    }
+    
+    /**
+     * Test method update
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->update(1, []));
+    }
+    
+    /**
+     * Test method delete
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->delete(1));
+    }
+}
