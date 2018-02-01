eac8503d09e51054d33a80fcc92aba2b967190d0
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-01 13:58:21 +0000

    CategoryTest class created with CRUD test methods

---
+++ tests/Categories/CategoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PrimeiraMao\Categories\Category;
+use PrimeiraMao\Http\Response;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class CategoryTest extends TestCase
+{
+    /**
+     * Customer Instance
+     * 
+     * @return \PrimeiraMao\Categories\Category
+     */
+    public static function instance()
+    {
+        return new Category();
+    }
+    
+    /**
+     * Test instance account credentials
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Category::class, $this->instance());
+    }
+    
+    /**
+     * Test method all
+     *
+     * @return void
+     */
+    public function testAll()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->all());
+    }
+    
+    /**
+     * Test method find
+     *
+     * @return void
+     */
+    public function testFind()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->find(1));
+    }
+}
