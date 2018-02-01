2b44cbaf9b7f1b97a97379f8315a5d11378a5993
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-01 14:02:28 +0000

    TrademarkTest class created with CRUD test methods

---
+++ tests/Categories/TrademarkTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PrimeiraMao\Categories\Trademark;
+use PrimeiraMao\Http\Response;
+
+/**
+ * PrimeiraMao API
+ * 
+ * @type        library
+ * @version     0.0.1
+ * @package     life-code/primeira-mao
+ * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class TrademarkTest extends TestCase
+{
+    /**
+     * Customer Instance
+     * 
+     * @return \PrimeiraMao\Categories\Trademark
+     */
+    public static function instance()
+    {
+        return new Trademark(1);
+    }
+    
+    /**
+     * Test instance account credentials
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Trademark::class, $this->instance());
+    }
+    
+    /**
+     * Test method all
+     *
+     * @return void
+     */
+    public function testAll()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->all());
+    }
+    
+    /**
+     * Test method find
+     *
+     * @return void
+     */
+    public function testFind()
+    {
+        $this->assertInstanceOf(Response::class, $this->instance()->find(1));
+    }
+}
