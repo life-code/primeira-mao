18518c66e7172a5a3e2d3df41ec4425e3c1472d7
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-02 13:00:24 +0000

    Translation newspapers messages

--- src/Config/Translations/newspapers/en.php
+++ src/Config/Translations/newspapers/en.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
+    'title' => [
+        'não pode ficar em branco' => 'The title is required.',
+    ],
     
+    'description' => [
+        'não pode ficar em branco' => 'The description is required.',
+    ],
+    
+    'price' => [
+        'não é um número' => 'The price is invalid.',
+    ],
+    
+    'kind' => [
+        'não pode ficar em branco' => 'The type is required.',
+        'não está incluído na lista' => 'The type is not included in the list.',
+    ],
+    
+    'permalink' => [
+        'não pode ficar em branco' => 'The permalink is required.',
+    ],
 ];
--- src/Config/Translations/newspapers/pt-br.php
+++ src/Config/Translations/newspapers/pt-br.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
+    'title' => [
+        'não pode ficar em branco' => 'O título é obrigatório.',
+    ],
     
+    'description' => [
+        'não pode ficar em branco' => 'A descrição é obrigatória.',
+    ],
+    
+    'price' => [
+        'não é um número' => 'O preço é inválido.',
+    ],
+    
+    'kind' => [
+        'não pode ficar em branco' => 'O tipo é obrigatório.',
+        'não está incluído na lista' => 'O tipo não está incluído na lista.',
+    ],
+    
+    'permalink' => [
+        'não pode ficar em branco' => 'O link permanente é obrigatório.',
+    ],
 ];
