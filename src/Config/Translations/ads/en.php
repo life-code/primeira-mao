4170fcaa323e31d1d4da42cf1a43dcce86a5c512
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-02 12:22:38 +0000

    Translation messages added in ads

--- src/Config/Translations/ads/en.php
+++ src/Config/Translations/ads/en.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+    'title' => [
+        'não pode ficar em branco' => 'The title is required.',
+        'é muito curto (mínimo: 6 caracteres)' => 'Title is too short (minimum: 6 characters).',
+    ],
     
+    'price' => [
+        'não pode ficar em branco' => 'The price is required.',
+    ],
+    
+    'phone' => [
+        'não pode ficar em branco' => 'The phone is required.',
+    ],
 ];
--- src/Config/Translations/ads/pt-br.php
+++ src/Config/Translations/ads/pt-br.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+    'title' => [
+        'não pode ficar em branco' => 'O título é obrigatório.',
+        'é muito curto (mínimo: 6 caracteres)' => 'O título é muito curto (mínimo: 6 caracteres).',
+    ],
     
+    'price' => [
+        'não pode ficar em branco' => 'O preço é obrigatório.',
+    ],
+    
+    'phone' => [
+        'não pode ficar em branco' => 'O telefone é obrigatório.',
+    ],
 ];
