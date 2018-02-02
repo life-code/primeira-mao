bb3634b1be5f72bace815147b9e512e763be9030
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-02 13:16:04 +0000

    Translation profiles messages

--- src/Config/Translations/profiles/en.php
+++ src/Config/Translations/profiles/en.php
@@ -1,5 +1,33 @@
 <?php
 
 return [
+    'address.state' => [
+        'Estado inválido' => 'The status is invalid.',
+    ],
     
+    'address.city' => [
+        'Cidade inválida' => 'The city is invalid.',
+    ],
+    
+    'address.neighborhood' => [
+        'Bairro inválido' => 'The neighborhood is invalid.',
+    ],
+    
+    'user.email' => [
+        'já está em uso' => 'The email is already in use.',
+    ],
+    
+    'gender' => [
+        'não pode ficar em branco' => 'The gender is required.',
+        'não está incluído na lista' => 'The gender is not included in the list.',
+    ],
+    
+    'birthday' => [
+        'não pode ficar em branco' => 'The birthday is required.',
+        'não está incluído na lista' => 'The birthday is not included in the list.',
+    ],
+    
+    'nickname' => [
+        'Apelido Inválido. Use apenas letras, _ e -' => 'The nickname is invalid, use only (letters, _ and -).',
+    ],
 ];
--- src/Config/Translations/profiles/pt-br.php
+++ src/Config/Translations/profiles/pt-br.php
@@ -1,5 +1,33 @@
 <?php
 
 return [
+    'address.state' => [
+        'Estado inválido' => 'O estado é inválido.',
+    ],
     
+    'address.city' => [
+        'Cidade inválida' => 'A cidade é inválida.',
+    ],
+    
+    'address.neighborhood' => [
+        'Bairro inválido' => 'O bairro é inválido.',
+    ],
+    
+    'user.email' => [
+        'já está em uso' => 'O email já está em uso.',
+    ],
+    
+    'gender' => [
+        'não pode ficar em branco' => 'O gênero é obrigatório.',
+        'não está incluído na lista' => 'O gênero não está incluído na lista.',
+    ],
+    
+    'birthday' => [
+        'não pode ficar em branco' => 'O aniversário é obrigatório.',
+        'não está incluído na lista' => 'O aniversário não está incluído na lista.',
+    ],
+    
+    'nickname' => [
+        'Apelido Inválido. Use apenas letras, _ e -' => 'O apelido é inválido, use apenas (letras, _ e -).',
+    ],
 ];
