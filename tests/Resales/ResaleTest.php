56c856dc33a42cd3a0d3812c3e95ec3d1ece582f
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-02 13:29:37 +0000

    Update version

--- tests/Resales/ResaleTest.php
+++ tests/Resales/ResaleTest.php
@@ -9,7 +9,7 @@ use PrimeiraMao\Http\Response;
  * PrimeiraMao API
  * 
  * @type        library
- * @version     0.0.3
+ * @version     0.0.4
  * @package     life-code/primeira-mao
  * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
