7bbc49ab27497c33d3e7a0e2dcf4415de3176b17
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-02-05 17:02:38 +0000

    Update version

--- src/Contracts/Languages/Language.php
+++ src/Contracts/Languages/Language.php
@@ -6,7 +6,7 @@ namespace PrimeiraMao\Contracts\Languages;
  * PrimeiraMao API
  * 
  * @type        library
- * @version     0.0.4
+ * @version     0.0.5
  * @package     life-code/primeira-mao
  * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
--- src/Http/ErrorBag.php
+++ src/Http/ErrorBag.php
@@ -10,7 +10,7 @@ use PrimeiraMao\Languages\Language;
  * PrimeiraMao API
  * 
  * @type        library
- * @version     0.0.4
+ * @version     0.0.5
  * @package     life-code/primeira-mao
  * @copyright   Copyright (c) 2018 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
